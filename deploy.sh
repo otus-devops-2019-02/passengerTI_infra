75eba4e096f0c1b0ef34d8e50c4783b002995d85
Andrey V. Semenushkin <andrey@semenushkin.ru>
2019-03-26 12:37:03 +0300

    Add changes for Travis CI verifications [deploy.sh, install_mongodb.sh]

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-host='35.187.118.194'
-username='appuser'
-ssh_options='-i ~/.ssh/appuser'
+git clone -b monolith https://github.com/express42/reddit.git /home/appuser/reddit
 
-ssh $ssh_options $username@$host "git clone -b monolith https://github.com/express42/reddit.git /home/appuser/reddit"
+cd /home/appuser/reddit && bundle install
 
-ssh $ssh_options $username@$host "cd /home/appuser/reddit && bundle install"
-
-ssh $ssh_options $username@$host "cd /home/appuser/reddit && puma -d"
+cd /home/appuser/reddit && puma -d
 
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-host='35.187.118.194'
-username='appuser'
-ssh_options='-i ~/.ssh/appuser'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo \"deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse\" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 
-ssh $ssh_options $username@$host "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927"
-ssh appuser@$host "sudo bash -c 'echo \"deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse\" > /etc/apt/sources.list.d/mongodb-org-3.2.list'"
+sudo apt update
 
-ssh $ssh_options $username@$host "sudo apt update"
+sudo apt install -y mongodb-org
 
-ssh $ssh_options $username@$host "sudo apt install -y mongodb-org"
+sudo systemctl start mongod
 
-ssh $ssh_options $username@$host "sudo systemctl start mongod"
-
-ssh $ssh_options $username@$host "sudo systemctl enable mongod"
+sudo systemctl enable mongod
 
 
