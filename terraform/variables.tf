17dc37b2987e386d8a100868d05cbf5660e6cd77
Andrey V. Semenushkin <andrey@semenushkin.ru>
2019-04-17 09:19:22 +0300

    Work modules in terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,6 @@ variable project {
 
 variable region {
   description = "Region"
-  # Значение по умолчанию
   default = "europe-west1"
 }
 
@@ -20,10 +19,6 @@ variable private_key_path {
 variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
 }
 
 variable db_disk_image {
