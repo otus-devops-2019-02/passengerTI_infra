cc4a77600ed904fb565a0d009ace3b83a53252f0
Andrey V. Semenushkin <andrey@semenushkin.ru>
2019-04-20 01:24:58 +0300

    Remove mistakes in stage/main.tf

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -12,15 +12,19 @@ provider "google" {
 module "app" {
   source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
+  private_key_path = "${var.private_key_path}"
   zone            = "${var.zone}"
   app_disk_image  = "${var.app_disk_image}"
+  db_internal_ip = "${module.db.db_internal_ip}"
 }
 
 module "db" {
   source          = "../modules/db"
   public_key_path = "${var.public_key_path}"
+  private_key_path = "${var.private_key_path}"
   zone            = "${var.zone}"
   db_disk_image   = "${var.db_disk_image}"
+  app_internal_ip = "${module.app.app_internal_ip}"
 }
 
 module "vpc" {
