6e411b474f48f5d650977a7026f3de5c2cf29b03
Andrey V. Semenushkin <andrey@semenushkin.ru>
2019-05-11 01:47:16 +0300

    Terraform validate

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -4,21 +4,12 @@ provider "google" {
     region = "${var.region}"
 }
 
-//module "storage-bucket" {
-//    source = "SweetOps/storage-bucket/google"
-//    version = "0.1.1"
-//    name = ["storage-bucket-passenger","storage-bucket-passenger2"]
-//}
+module "storage-bucket" {
+    source = "SweetOps/storage-bucket/google"
+    version = "0.1.1"
+    name = ["storage-bucket-passenger","storage-bucket-passenger2"]
+}
 
 output storge-bucket_url {
     value = "${module.storage-bucket.url}"
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "storage-bucket-passenger"
-  force_destroy = "true"
-
-  versioning {
-    enabled = "true"
-  }
 }
